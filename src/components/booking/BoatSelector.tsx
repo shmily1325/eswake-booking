b910f699a0d9e81015478fb4acf90a36c90e6fa8
Ming Lin <limin@nvidia.com>
2025-11-24 15:56:13 +0800

    fix: remove unused React imports

--- src/components/booking/BoatSelector.tsx
+++ src/components/booking/BoatSelector.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import type { Boat } from '../../types/booking'
 
 interface BoatSelectorProps {
--- src/components/booking/TimeSelector.tsx
+++ src/components/booking/TimeSelector.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 interface TimeSelectorProps {
     startDate: string
     setStartDate: (date: string) => void
